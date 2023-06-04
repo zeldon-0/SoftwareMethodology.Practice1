0dd112b4837fbb35af4045f54d2987cf3dfef3fc
Vadym Lavrinenko <vadym.lavrinenko@ihsmarkit.com>
2023-06-04 21:23:41 +0300

    Remove the unnecessary try-catch block

--- SoftwareMethodology.Practice1/Program.cs
+++ SoftwareMethodology.Practice1/Program.cs
@@ -8,15 +8,8 @@ try
     foreach (var testCase in testCases)
     {
         Console.WriteLine($"Case Number {testCase.Number}");
-        try
-        {
-            var grid = new Map(testCase.Countries.ToList());
-            Console.Write(grid.RunSimulation());
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e.Message);
-        }
+        var grid = new Map(testCase.Countries.ToList());
+        Console.Write(grid.RunSimulation());
     }
 }
 catch (Exception e)
